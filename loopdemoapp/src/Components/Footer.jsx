ff165e9decef62050cbfc6816f7ffe63e1f7be82
shubham verma <sv7836832607@gmail.com>
2024-08-02 16:15:12 +0530

    footer font color change

--- loopdemoapp/src/Components/Footer.jsx
+++ loopdemoapp/src/Components/Footer.jsx
@@ -26,7 +26,7 @@ export default function Footer() {
   return (
     <Box
       bg={'#0d062b'}
-      color={useColorModeValue('gray.700', 'gray.200')}>
+      color={useColorModeValue('gray.100', 'gray.200')}>
       <Container as={Stack} maxW={'6xl'} py={10}>
         <SimpleGrid columns={{ base: 1, sm: 2, md: 4 }} spacing={8}>
           <Stack align={'flex-start'}>
